2c5f530bf9f1a4ec2be18c524cd23850fc95c295
Nikhil <kumarnikhil48578@gmail.com>
2026-04-14 12:11:49 +0530

    feat: add drizzel and better-auth

---
+++ lib/auth.ts
@@ -0,0 +1,29 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+//@ts-ignore
+import { schema } from "@/db/schema/auth-schema";
+
+
+export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+    basePath: "/api/auth",
+    trustedOrigins: [
+        "http://localhost:3000",
+    ],
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema: schema,
+    }),
+
+    socialProviders: {
+        google: {
+            accessType: "offline",
+            prompt: "select_account consent",
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env
+                .GOOGLE_CLIENT_SECRET as string,
+        },
+    },
+
+});
